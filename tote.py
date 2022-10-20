6b33e71350d2257294425cb132d08ad7e8d8ba88
ATVQ (Attila Vural) <ATVQ@novonordisk.com>
2022-10-20 12:58:50 +0200

    added prev button functionality

--- tote.py
+++ tote.py
@@ -1,35 +1,40 @@
-# Status: works fine. Now find out how to pass either -1 or 1 into the function from the button call, so you 
-#         dont have to make two functions (one or increment verse/ayah and one for decrement).
-#         Next make a button to do the same thing with the verse number.
-
-
-
 import requests
-#import pandas as pd
-from pprint import pprint
 import tkinter as tk
 
-class ayah():
-    def write_slogan(self,direction):
-        if not hasattr(write_slogan, "counter"):
-            write_slogan.counter = 0  # it doesn't exist yet, so initialize it
-            print("initalized")
-        write_slogan.counter += 1
-        print(write_slogan.counter)
-        url = "http://api.alquran.cloud/v1/ayah/0"+str(write_slogan.verse)+str(write_slogan.counter)
+class window():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.counter = 0
         
+    def run(self):
+        self.root.mainloop()
+    
+    def create_label(self):
+        self.myLabel = tk.Label(self.root, text="")
+        self.myLabel.grid(row=2, column=1)
+    
+    def create_button_next(self):
+        btn_next = tk.Button(self.root, text='next', command = lambda : self.update_label_text(1))
+        btn_next.grid(row=3, column=1, sticky=tk.W, pady=4)
+        
+    def create_button_prev(self):
+        btn_prev = tk.Button(self.root, text='prev', command = lambda : self.update_label_text(-1))
+        btn_prev.grid(row=3, column=6, sticky=tk.W, pady=4)
+    
+    def update_label_text(self,direction):
+        self.counter += direction
+        url = "http://api.alquran.cloud/v1/ayah/0"+str(2)+str(self.counter)
+        # Retrieve text string
         response = requests.request("GET", url).json()
         info_message = response["data"]["text"]
         # Insert the text into the window
-        tk.Label(master, text=info_message).grid(row=2, column=1)
+        self.myLabel.config(text=info_message)
+        print(self.counter)
+    
+a = window()
+a.create_label()
+a.create_button_next()
+a.create_button_prev()
+a.run()
 
-# Instantiate the tinker object
-master = tk.Tk()
 
-
-write_slogan.verse = 2
-btn = tk.Button(master, text='next', command=write_slogan)
-btn.grid(row=3, column=1, sticky=tk.W, pady=4)
-
-# Show the window
-master.mainloop()
